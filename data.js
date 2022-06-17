a2455f9c0483029a72a6daaa1e644368c3be4ea1
Chad Clough <cclough@HB.local>
2022-06-17 15:42:34 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,269 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-7w33-meds-room",
+      "name": "7W33 MEDS ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": -0.7953088443376064,
+        "pitch": 0.46442363943779164,
+        "fov": 1.3721846102296735
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23889857344011922,
+          "pitch": 0.5344488380254582,
+          "rotation": 4.71238898038469,
+          "target": "1-7w32-clean-supply"
+        },
+        {
+          "yaw": -1.5740482221265601,
+          "pitch": 0.3466277885661402,
+          "rotation": 0,
+          "target": "2-7w29-nurse-station"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-7w32-clean-supply",
+      "name": "7W32 CLEAN SUPPLY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": -3.045026424074665,
+        "pitch": 0.2915363704996832,
+        "fov": 1.3721846102296735
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.445376197201906,
+          "pitch": 0.5203436373421866,
+          "rotation": 3.141592653589793,
+          "target": "0-7w33-meds-room"
+        },
+        {
+          "yaw": 2.669203722296902,
+          "pitch": 0.3634099505063624,
+          "rotation": 0.7853981633974483,
+          "target": "2-7w29-nurse-station"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-7w29-nurse-station",
+      "name": "7W29 NURSE STATION",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": 0.4408067501230555,
+        "pitch": 0.3856096192806575,
+        "fov": 1.3721846102296735
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5509607802517955,
+          "pitch": 0.5537992528795552,
+          "rotation": 4.71238898038469,
+          "target": "0-7w33-meds-room"
+        },
+        {
+          "yaw": 2.8426823606321765,
+          "pitch": 0.4006441361175366,
+          "rotation": 3.141592653589793,
+          "target": "1-7w32-clean-supply"
+        },
+        {
+          "yaw": 0.4022578132342911,
+          "pitch": 0.3523146008123259,
+          "rotation": 0,
+          "target": "3-7w25-ante-room"
+        },
+        {
+          "yaw": 0.31521020889992357,
+          "pitch": 0.22921384293824154,
+          "rotation": 0,
+          "target": "4-7w10-inpatient-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-7w25-ante-room",
+      "name": "7W25 ANTE ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": -1.4782197831951294,
+        "pitch": 0.42497409401595654,
+        "fov": 1.3721846102296735
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.67442839905791,
+          "pitch": 0.2449649361824573,
+          "rotation": 4.71238898038469,
+          "target": "0-7w33-meds-room"
+        },
+        {
+          "yaw": 1.2686616269785063,
+          "pitch": 0.20878121165016061,
+          "rotation": 3.141592653589793,
+          "target": "1-7w32-clean-supply"
+        },
+        {
+          "yaw": 1.6043711137097434,
+          "pitch": 0.42584037182844625,
+          "rotation": 3.141592653589793,
+          "target": "2-7w29-nurse-station"
+        },
+        {
+          "yaw": -1.4091435537309192,
+          "pitch": 0.7313939823382505,
+          "rotation": 0,
+          "target": "4-7w10-inpatient-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-7w10-inpatient-room",
+      "name": "7W10 INPATIENT ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": -1.357806582039764,
+        "pitch": 0.19278856771142472,
+        "fov": 1.3721846102296735
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1484843991847065,
+          "pitch": 0.5266835023416263,
+          "rotation": 9.42477796076938,
+          "target": "3-7w25-ante-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "L07Test3",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
